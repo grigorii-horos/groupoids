11cdc2c3c706454ac0ee7d98eccc6c27f2fa4bd0
Grigorii Horos <horosgrisa@gmail.com>
2019-02-21 15:51:32 +0200

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { numToMatrix } = require('./lib/utils');
 // const isSemigroup = require('./lib/isSemigroup');
 const isMoufang = require('./lib/isMoufang');
 
-const n = 4;
+const n = 3;
 
 
 for (let i = 0; i < n ** (n * n); i += 1) {
