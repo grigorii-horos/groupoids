adf1bfa14189b835b0c8e63f2c85cb36891c6510
Grigorii Horos <horosgrisa@gmail.com>
2019-03-19 21:52:46 +0200

    Add functions

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-
 const { numToMatrix } = require('./lib');
 
-// const isSemigroup = require('./lib/isSemigroup');
-// const isMoufang = require('./lib/isMoufang');
-// const isLeftBol = require('./lib/isLeftBol');
-// const isRightBol = require('./lib/isRightBol');
-// const isAbelianGroup = require('./lib/isAbelianGroup');
-// const isCLoop = require('./lib/isCLoop');
-// const isMiddleNuclearSquare = require('./lib/isMiddleNuclearSquare');
-// const isRightNuclearSquare = require('./lib/isRightNuclearSquare');
-const isLeftNuclearSquare = require('./lib/isLeftNuclearSquare');
+
+const fn = require('./lib/isMateI');
 
 const n = 4;
 
@@ -17,8 +9,7 @@ const n = 4;
 for (let i = 0; i < n ** (n * n); i += 1) {
   const matrix = (numToMatrix(n)(i));
 
-
-  if (isLeftNuclearSquare(matrix, n)) {
+  if (fn(matrix, n)) {
     console.log(i);
   }
 }
